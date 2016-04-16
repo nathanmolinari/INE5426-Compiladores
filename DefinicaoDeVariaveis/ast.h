2ca6cee244c0e45521421e03f129131c84b3a3a7
ArthurHenriqueDellaFraga <Arthur.Henrique.Della.Fraga@gmail.com>
2016-04-16 17:05:36 -0300

    Estruturação da Análise sintatica

--- DefinicaoDeVariaveis/ast.h
+++ DefinicaoDeVariaveis/ast.h
@@ -18,27 +18,64 @@ typedef std::vector<Node*> NodeList; //List of ASTs
 
 class Node {
     public:
-        virtual ~Node() {}
-        virtual void printTree(){}
-        virtual int computeTree(){return 0;}
+        virtual ~Node() { }
+        virtual void printTree() { }
+//        virtual int computeTree(){return 0;}
 };
 
-class Integer : public Node {
-    public:
-        int value;
-        Integer(int value) : value(value) {  }
-        void printTree();
-        int computeTree();
-};
+class Primitivo : public Node { }
 
-class Identificador : public Node {
-    public:
-        string identificador;
-        Identificador(std::string identificador) :
-            identificador(identificador) { }
-        void printTree();
-        int computeTree();
-};
+  class Integer : public Primitivo {
+      public:
+          int value;
+          Integer(int value) : value(value) { }
+          void printTree();
+  };
+
+  class Racional : public Primitivo {
+      public:
+          double value;
+          Racional(double value) : value(value) { }
+          void printTree();
+  }
+
+  class Caracter : public Primitivo {
+      public:
+          char value;
+          Caracter(char value) : value(value) { }
+          void printTree();
+  }
+
+  class Sentenca : public Primitivo {
+      public:
+          string value;
+          Sentenca(string value) : value(value) { }
+          void printTree();
+  }
+
+  class Booleano : public Primitivo {
+      public:
+          bool value;
+          Booleano(bool value) : value(value) { }
+          void printTree();
+  }
+
+class Complexo : public Node { }
+
+  class Identificador : public Complexo {
+      public:
+          string identificador;
+          Identificador(std::string identificador) :
+              identificador(identificador) { }
+          void printTree();
+          int computeTree();
+  };
+
+class Operacao : public Node { }
+
+class Soma : public Operacao {
+    
+}
 
 class BinOp : public Node {
     public:
